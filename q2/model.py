db1016694b4f9e5471fe794937b47622b388b846
Arijit Gupta <arijitgupta2000@gmail.com>
2019-08-06 15:50:45 +0530

    Converting labels to tensors

--- q2/model.py
+++ q2/model.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy
 
 import torch
 from torch import nn
@@ -12,28 +13,75 @@ import os
 
 import time
 
+import pickle
+
 import cv2
 from os.path import isfile,join
 
 VideoLoad = videoDataset()
 
-samples=[]
-
 n = 0
 
+with open("D:/UCF11/fileName.pickle", "rb") as fp:  
+           clipsList = pickle.load(fp) 
+length = len(clipsList)
+
+print(clipsList)
+labellist=[]
+
+for i in range(length):
+    if(clipsList[i][1] == 'shooting'):
+        clipsList[i][1] = 0
+    if(clipsList[i][1] == 'biking'):
+        clipsList[i][1] = 1
+    if(clipsList[i][1] == 'diving'):
+        clipsList[i][1] = 2
+    if(clipsList[i][1] == 'golf'):
+        clipsList[i][1] = 3
+    if(clipsList[i][1] == 'riding'):
+        clipsList[i][1] = 4
+    if(clipsList[i][1] == 'juggle'):
+        clipsList[i][1] = 5
+    if(clipsList[i][1] == 'swing'):
+        clipsList[i][1] = 6
+    if(clipsList[i][1] == 'tennis'):
+        clipsList[i][1] = 7
+    if(clipsList[i][1] == 'jumping'):
+        clipsList[i][1] = 8
+    if(clipsList[i][1] == 'spiking'):
+        clipsList[i][1] = 9
+    if(clipsList[i][1] == 'walk_dog'):
+        clipsList[i][1] = 10
+    labellist.append(clipsList[i][1])
+
+train_data = []
+test_data = []
+
+
+ 
+
+
 for filename in os.listdir("D:/UCF11/data"):
-	for filename2 in os.listdir("D:/UCF11/data/"+filename):
-		for filename3 in os.listdir("D:/UCF11/data/"+filename+'/'+filename2):
-			if(filename3.split(".")[0].split("_")[-1] == 'resized'):
-					sample = videoDataset.readVideo(VideoLoad, "D:/UCF11/data/"+filename+'/'+filename2+'/'+filename3)
-					n = n + 1
-					if(n%3==0):
-						testloader = torch.utils.data.DataLoader(sample[0])
-						print("Loaded to testloader!")
-						
-					else:
-						trainloader = torch.utils.data.DataLoader(sample[0])
-						print("Loaded to trainloader!")
+    for filename2 in os.listdir("D:/UCF11/data/"+filename):
+        for filename3 in os.listdir("D:/UCF11/data/"+filename+'/'+filename2):
+            if(filename3.split(".")[0].split("_")[-1] == 'resized'):
+                    sample = videoDataset.readVideo(VideoLoad, "D:/UCF11/data/"+filename+'/'+filename2+'/'+filename3)
+                    
+                    if(n%3==0):
+                        train_data.append([sample[0], torch.from_numpy(numpy.array(labellist[n]))])
+                        print(sample[0].shape)
+                                                
+                    else:
+                        test_data.append([sample[0], torch.from_numpy(numpy.array(labellist[n]))])
+                        print(sample[0].shape)
+                        
+                    n = n + 1
+
+trainloader = torch.utils.data.DataLoader(train_data, shuffle=True)
+print("Loaded to trainloader...")
+testloader = torch.utils.data.DataLoader(test_data, shuffle=True)
+print("Loaded to testloader...")
+
 model = models.densenet121(pretrained=True)
 for param in model.parameters():
     param.requires_grad = False
@@ -63,8 +111,9 @@ print_every = 5
 for epoch in range(epochs):
     for inputs, labels in trainloader:
         steps += 1
+               
         inputs, labels = inputs.to(device), labels.to(device)
-        
+
         optimizer.zero_grad()
         
         logps = model.forward(inputs)
@@ -96,4 +145,4 @@ for epoch in range(epochs):
                   f"Test loss: {test_loss/len(testloader):.3f}.. "
                   f"Test accuracy: {accuracy/len(testloader):.3f}")
             running_loss = 0
-            model.train()
+            model.train()  
